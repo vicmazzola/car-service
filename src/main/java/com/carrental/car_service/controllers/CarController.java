b664c4ad55082e5e2cb1de81e785b74ddb97f303
Victor <victormazzola2019@gmail.com>
2026-03-20 18:14:06 -0300

    feat: implement car query endpoints
    - Add GET /cars with pagination support
    - Add GET /cars/{id} endpoint
    - Integrate CarService layer
    - Add basic logging

--- src/main/java/com/carrental/car_service/controllers/CarController.java
+++ src/main/java/com/carrental/car_service/controllers/CarController.java
@@ -1,15 +1,52 @@
 package com.carrental.car_service.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.carrental.car_service.entities.Car;
+import com.carrental.car_service.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
 
-    @GetMapping
-    public String getCars() {
-        return "Hello there!";
+    private static final Logger logger = LoggerFactory.getLogger(CarController.class);
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
+
+
+    // http://localhost:8080/cars?page=1&&size=10
+
+    @GetMapping
+    public ResponseEntity<List<Car>> findAllCars(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size) {
+        logger.info("/cars");
+
+        var cars = this.carService.findAllCars(page, size);
+
+        return ResponseEntity.ok(cars);
+    }
+
+    // http://localhost:8080/cars/1
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Car>> findCar(
+            @PathVariable("id") Long id
+    ) {
+        logger.info("/cars/" + id);
+
+        var car = this.carService.findCarById(id);
+
+        return ResponseEntity.ok(car);
+    }
+
 }
