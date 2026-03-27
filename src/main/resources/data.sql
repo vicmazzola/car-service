cf79f84493f66b7d0405bf1627ff4a99b9ae3459
Victor <victormazzola2019@gmail.com>
2026-03-27 10:18:58 -0300

    feat: add sample data to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -30,8 +30,17 @@ create TABLE IF NOT EXISTS rent (
 insert into cars (brand, model, car_year, color, daily_price) values
 ('Porsche', 'Carrera', 2012, 'Yellow', 100.00);
 
+insert into cars (brand, model, car_year, color, daily_price) values
+('Ferrari', 'Testarossa', 1980, 'Red', 500.00);
+
 insert into customers (name, cpf, phone, email) values
 ('Victor', '47116978844', '11974874744', 'victor@gmail.com');
 
+insert into customers (name, cpf, phone, email) values
+('Emilia', '47116978877', '11974874678', 'emilia@gmail.com');
+
 insert into rent(customer_id, car_id, initial_date, end_date, total_value) values
-(1, 1, '2026-10-1', '2026-10-15', 1500.00 )
+(1, 1, '2026-10-1', '2026-10-15', 1500.00 );
+
+insert into rent(customer_id, car_id, initial_date, end_date, total_value) values
+(2, 2, '2026-06-1', '2026-06-15', 2500.00 );
